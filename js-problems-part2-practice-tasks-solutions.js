254420e8b0883b9668000111aa2bfb572da12ff2
Mohammad-Rofikul-Islam-Hasan <rofikulislam.ph@gmail.com>
2025-08-13 05:55:52 +0600

    Task-2 Solution Added

--- js-problems-part2-practice-tasks-solutions.js
+++ js-problems-part2-practice-tasks-solutions.js
@@ -12,3 +12,19 @@ function getLowestNumber(arr) {
 }
 
 console.log(getLowestNumber(heights2));
+
+// Task 2 Solution
+
+const friends = ["rahim", "robin", "rafi", "ron", "rashed"];
+
+function getSmallestName(arr) {
+  let smallestName = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].length < smallestName.length) {
+      smallestName = arr[i];
+    }
+  }
+  return smallestName;
+}
+
+console.log(getSmallestName(friends));
