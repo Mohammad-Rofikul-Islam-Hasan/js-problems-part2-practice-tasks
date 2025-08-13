b5093badcfaa62f1eb97306927501a5e21acc102
Mohammad-Rofikul-Islam-Hasan <rofikulislam.ph@gmail.com>
2025-08-13 06:17:33 +0600

    Task-4 Solution Added

--- js-problems-part2-practice-tasks-solutions.js
+++ js-problems-part2-practice-tasks-solutions.js
@@ -42,3 +42,26 @@ function calculateElectronicsBudget(laptopQty, tabletQty, mobileQty) {
 }
 
 console.log(calculateElectronicsBudget(2, 1, 3));
+
+// Task 4 Solution
+
+const phones = [
+            { model: "PhoneA", brand: "Iphone", price: 95000 },
+            { model: "PhoneB", brand: "Samsung", price: 40000 },
+            { model: "PhoneC", brand: "Oppo", price: 26000 },
+            { model: "PhoneD", brand: "Nokia", price: 35000 },
+            { model: "PhoneE", brand: "Iphone", price: 105000 },
+            { model: "PhoneF", brand: "HTC", price: 48000 },
+        ];
+
+function findAveragePhonePrice (arr){
+  let totalPrice = 0;
+  
+  for (let phone of arr){
+  totalPrice = totalPrice + phone.price;
+  }
+  const averagePrice = Number((totalPrice / arr.length).toFixed(2));
+  return averagePrice;
+}
+
+console.log(findAveragePhonePrice (phones))
